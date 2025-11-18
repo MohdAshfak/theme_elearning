0f8ae252a21808ab96c3e29ccb07605600f1e955
MohdAshfak <ashfakmuhammed704@gmail.com>
2025-11-18 15:28:53 +0530

    updated

--- theme_elearning/__manifest__.py
+++ theme_elearning/__manifest__.py
@@ -4,10 +4,29 @@
     'category': 'Theme',
     'version': '18.0.1.0.0',
     'sequence': 1,
+    'author': 'XAPP',
+    'summary': """Unlock the full potential of your eLearning platform with this modern, fully customizable Odoo website theme—designed exclusively for online academies, coaching centers, instructors, corporate training providers, and digital learning platforms.
+                  With a clean UI, intuitive course layout system, and student-friendly experience, this theme makes it effortless to build a professional learning website that boosts engagement and reflects your brand identity.
+                   """,
+
+    'description': """
+       E-Learning - eLearning Website Theme for Odoo 18
+
+      A powerful, elegant, and fully responsive eLearning website theme crafted for online education providers,
+tutors, training institutes, coaching platforms, and professional learning businesses.
+
+       Support:
+       -
+       For any questions or support, please contact.
+   """,
+    'license': 'OPL-1',
+    'support': 'teamxapp.dev@gmail.com',
+    'price': 49.11,
+    'currency': 'USD',
+
     'depends': [
         'website',
         'web_editor',
-        'crm',
     ],
 
     'data': [
@@ -44,13 +63,12 @@
         ],
     },
 
-'images': [
-        'static/description/eLearning_landscapes.gif',
-        'static/description/elearning_protriates.gif',
+    'images': [
+        'static/description/elearning_cover.gif',
+        'static/description/elearning_screenshot.gif',
 
     ],
 
-    'live_test_url': 'http://13.60.52.65:8071',
     'installable': True,
     'auto_install': False,
     'application': False,
