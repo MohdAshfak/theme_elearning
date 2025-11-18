9d92074f8b466cb1a201940bfdb91fcc0b2ab1b3
MUHAMMMED ASHFAK <muhammedashfak84@gmail.com>
2025-11-18 15:32:59 +0530

    price updatedd

--- theme_elearning/__manifest__.py
+++ theme_elearning/__manifest__.py
@@ -21,7 +21,7 @@ tutors, training institutes, coaching platforms, and professional learning busin
    """,
     'license': 'OPL-1',
     'support': 'teamxapp.dev@gmail.com',
-    'price': 49.11,
+    'price': 39.11,
     'currency': 'USD',
 
     'depends': [
